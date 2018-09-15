ea186120ffbd031597c828539edeffceb46c9733
Sarah Harrington <sarah.soberg@gmail.com>
2018-09-14 22:20:56 -0500

    added comments, refactored a bit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,8 @@
 console.log('JS loaded');
+// Socket
 const socket = io();
 
+// ============ Variables ============ 
 const numBtns = [...document.querySelectorAll('.num-btn')];
 const clearBtn = document.querySelector('#clear');
 const backBtn = document.querySelector('#back');
@@ -9,47 +11,42 @@ const equalBtn = document.querySelector('.equals');
 const history = document.querySelector('.history');
 let calculation = [];
 
-numBtns.forEach((btn) => {
-  btn.addEventListener('click', number);
-})
-
+// ============ SOCKET EVENTS ============ 
+// When a new client connects, takes calcHistory and displays
 socket.on('newClientConnected', (calcHistory) => {
-  console.log('a new client has connected', calcHistory);
-  calcHistory.forEach(calc => {
-    let indCalc = document.createElement('p');
-    indCalc.innerHTML = calc;
-    history.appendChild(indCalc);
-  })
+  console.log('a new client has connected');
+  updateCalcHistory(calcHistory);
 })
 
+//After calculation is complete, updates display with calc result
 socket.on('calculationDone', (data) => {
-  display.innerHTML = `${data}`
+  display.innerHTML = `${data}`;
 })
 
+//After the calculation is done, updates the history display
 socket.on('updateHistory', (calcHistory) => {
   history.innerHTML = '';
-  calcHistory.forEach(calc => {
-    let indCalc = document.createElement('p');
-    indCalc.innerHTML = calc;
-    history.appendChild(indCalc);
-  })
+  updateCalcHistory(calcHistory);
 })
 
+// ============ FUNCTIONS ============ 
+//collects the nubmers and operators and pushes them to array
 function number(e) {
   let btn = e.target.id;
   calculation.push(btn);
-  console.log(calculation);
   updateDisplay();
 }
 
+//calculates the string after checking to make sure string is valid
 function calculateTheString() {
   if (calculation.length > 1) {
-    let a = calculation[calculation.length - 1];
-    let b = calculation[0];
+    let a = calculation[calculation.length - 1]; //last button pushed
+    let b = calculation[0]; //first button pushed
     if ((a === '/' || a === '*' || a === '-' || a === '+') || (b === '/' || b === '*' || b === '+')) {
       return;
     }
     else {
+      //if conditions are met, sent to server
       socket.emit('calculationTime', calculation);
       calculation = [];
     }
@@ -70,6 +67,19 @@ function backButton() {
   updateDisplay();
 }
 
+//updates the calculation history on page load or new calc is done
+function updateCalcHistory(calcHistory) {
+  calcHistory.forEach(calc => {
+    let indCalc = document.createElement('p');
+    indCalc.innerHTML = calc;
+    history.appendChild(indCalc);
+  })
+}
+
+// ============ EVENT LISTENERS ============ 
+numBtns.forEach((btn) => {
+  btn.addEventListener('click', number);
+})
 equalBtn.addEventListener('click', calculateTheString)
 clearBtn.addEventListener('click', clearCalculator);
 backBtn.addEventListener('click', backButton);
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+//requirements etc.
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 5000;
 const math = require('mathjs');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
+//express
 app.use(express.static(`${__dirname}/public`));
 
 //Mongoose for the DB
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 let mongoURI = '';
 if (process.env.MONGODB_URI != undefined) {
   mongoURI = process.env.MONGODB_URI;
@@ -21,33 +23,41 @@ mongoose.connection.on('connected', () => console.log('mongoose is connected'))
 let calcSchema = new Schema({ calc: String });
 let Calc = mongoose.model('Calculation', calcSchema, 'calculations');
 
-//The Goodstuf
+//The Good Stuff
 let calcHistory = [];
 let calculation = '';
 
-io.on('connection', (socket) => {
-  console.log('a new client has connected', calcHistory);
-  console.log(socket.id);
 
+// ==== SOCKET ====
+io.on('connection', (socket) => {
+  //WHEN NEW CLIENT CONNECTS, SENDS CALC HISTORY
   socket.emit('newClientConnected', calcHistory);
+
+  //LISTENING FOR CALCULATION FROM CLIENT
   socket.on('calculationTime', (data) => {
-    console.log('calculation socket id', socket.id);
     calculateTotal(data, socket.id);
   })
+
   getCalcs();
 })
 
+// ==== FUNCTIONS ====
 function calculateTotal(data, socketId) {
   calculation = data.join('');
   let mathTotal = math.eval(calculation);
   calculation = calculation + '=' + mathTotal;
-  // calcHistory.unshift(calculation);
-  // calcHistory = [...calcHistory.slice(0, 10)];
+
+  //EMITS THE TOTAL BACK TO THE CLIENT THAT SENT THE REQUEST
   io.to(socketId).emit('calculationDone', mathTotal);
+
+  //EMITS TO EVERYONE CONNECTED TO UDPATE HISTORY
   io.emit('updateHistory', calcHistory);
+
+  //SAVES CALC TO DATABASE
   saveCalc(calculation);
 }
 
+//SAVES THE CALCULATION TO THE DATABASE
 function saveCalc(calculation) {
   let calcToSave = new Calc({ calc: calculation });
   calcToSave.save((err, data) => {
@@ -55,19 +65,18 @@ function saveCalc(calculation) {
       console.log('Save Error');
     }
     else {
-      console.log('item saved')
       getCalcs();
     }
   })
 }
 
+//GETS THE CALCULATIONS FROM THE MONGO
 function getCalcs() {
   Calc.find({}, null, { sort: { _id: -1 } }, (err, calculations) => {
     if (err) {
       console.log('Error Getting Calcs')
     }
     else {
-      console.log(calculations);
       calcHistory = [];
       calculations.forEach((calc) => {
         calcHistory.push(calc.calc);
